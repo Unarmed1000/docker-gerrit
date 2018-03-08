4a0cd9eef5ebfe48d6267e069f0b337a7bf41b59
Rene Thrane <rene.thrane@manabattery.com>
2018-03-08 01:49:34 +0100

    Haveged

--- gerrit-start.sh
+++ gerrit-start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env sh
 set -e
-echo "Running haveged for additional entropy for Cloud Servers, this should help gerrit start faster".
-haveged -w 1024 &
 echo "Starting Gerrit..."
 exec su-exec ${GERRIT_USER} ${GERRIT_SITE}/bin/gerrit.sh ${GERRIT_START_ACTION:-daemon}
